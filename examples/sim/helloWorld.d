76af818dda421f548e0dc77764696beb57dd7f26
Puneet Goel <puneet@coverify.com>
2014-04-30 22:18:27 +0530

    Purged boilerplate from helloWorld.

--- examples/sim/helloWorld.d
+++ examples/sim/helloWorld.d
@@ -3,14 +3,12 @@
 //            (See accompanying file LICENSE_1_0.txt or copy at
 //            http://www.boost.org/LICENSE_1_0.txt)
 // Authors:   Puneet Goel <puneet@coverify.com>
-
 import esdl.base;
 
 class Hello: Entity {
   void sayHello() {
     import std.stdio: writeln;
-    writeln("Hello World from: ", getFullName(), ":",
-	    getRootEntity.getTimePrecision().normalize());
+    writeln("Hello World from: ", getFullName());
   }
   Task!sayHello greet[2];
 }
@@ -18,10 +16,6 @@ class Hello: Entity {
 class VlangWorld: RootEntity {
   this(string name) {
     super(name);
-  }
-  override void doConfig() {
-    timePrecision = 1.psec;
-    timeUnit = 10.nsec;
   }
   Hello hello[2];
 }
