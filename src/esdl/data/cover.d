71f161b0d0f67a715d67d60cec91bb375a0dbac1
Puneet Goel <puneet@coverify.com>
2017-03-01 09:42:28 +0530

    added logic for cover bins

--- src/esdl/data/cover.d
+++ src/esdl/data/cover.d
@@ -14,27 +14,119 @@ module esdl.data.cover;		// Coverage
 struct CoverPoint(alias t, string BINS="") {
   import std.traits: isIntegral;
   import esdl.data.bvec: isBitVector;
-  pragma(msg, __traits(parent, t));
   alias T = typeof(t);
   static assert(isIntegral!T || isBitVector!T || is(T: bool),
 		"Only integral, bitvec, or bool values can be covered."
 		~ " Unable to cover a value of type: " ~ T.stringof);
+  T* _point; // = &t;
   // the number of bins and which one is hit is made out by the
   // sample function
-  int[] bins;    // We keep a count of how many times a bin is hit
+  int[] _bins;	     // We keep a count of how many times a bin is hit
+  int _pos;	     // position of t the covergoup; -1 otherwise
+  void _initPoint(G)(G g) {
+    auto _outer = g.outer;
+    assert (_outer !is null);
+    static if (__traits(hasMember, g, t.stringof)) {
+      _point = &(__traits(getMember, g, t.stringof));
+      assert(_point !is null);
+    }
+    else static if (__traits(hasMember, g.outer, t.stringof)) {
+      _point = &(__traits(getMember, _outer, t.stringof));
+      assert(_point !is null);
+    }
+    else {
+      _point = &(t);
+      assert(_point !is null);
+    }
+    static if (isIntegral!T) {
+      _bins.length = 64;
+    }
+    else static if (is(T == bool)) {
+      _bins.length = 2;
+    }
+    else {
+      static if (T.SIZE > 6) {
+	_bins.length = 64;
+      }
+      else {
+	_bins.length = T.max - T.min;
+      }
+    }
+  }
+  
+  void _initPos(int pos) {
+    _pos = pos;
+  }
+
+  auto getBins() {
+    return _bins;
+  }
+  
+  static if(BINS == "") {
+    void sample(int argc) {
+      if (_pos >= argc) {}
+      else {
+	static if (isIntegral!T) {
+	  size_t range = cast(size_t) T.max - cast(size_t) T.min;
+	  size_t size = (range/2 + 1)/32;
+	  size_t slot = (cast(size_t) *_point - cast(size_t) T.min)/size;
+	  import std.conv;
+	  assert(slot < _bins.length, "slot is: " ~ slot.to!string);
+	  assert(slot >= 0);
+	  _bins[slot]++;
+	}
+      }
+    }
+  }
 }
 
 class CoverGroup {		// Base Class
+  bool _isInitialized;		// true if the CoverGroup has been initialized
 }
+
+private void initialize(G, int I=0)(G g) if (is(G: CoverGroup)) {
+  static if (I == 0) {
+    if (g._isInitialized) return;
+    else g._isInitialized = true;
+  }
+  static if (I >= G.tupleof.length) {
+    return;
+  }
+  else {
+    alias E = typeof(g.tupleof[I]);
+    static if (is (E: CoverPoint!(t, S), alias t, string S)) {
+      g.tupleof[I]._initPoint(g);
+      int index = findElementIndex!(t.stringof, G);
+      g.tupleof[I]._initPos(index);
+    }
+    initialize!(G, I+1)(g);
+  }
+ }
+
+private void samplePoints(int I, int N, G)(G g) if (is(G: CoverGroup)) {
+  static if (I >= G.tupleof.length) {
+    return;
+  }
+  else {
+    alias E = typeof(g.tupleof[I]);
+    static if (is (E: CoverPoint!(t, S), alias t, string S)) {
+      g.tupleof[I].sample(N);
+    }
+    samplePoints!(I+1, N)(g);
+  }
+ }
+
 
 public void sample(G, V...)(G g, V v) if (is(G: CoverGroup)) {
   // navigate through the class elements of G to know the CoverPoint
   // instantiations as well as any Integral/BitVector instances
-  sample_args!(0)(g, v);
+  sampleArgs!(0)(g, v);
+  initialize(g);
   // Now look for all the coverpoints
+  samplePoints!(0, V.length)(g);
  }
 
-private void sample_args(int I, G, V...)(G g, V v) {
+private void sampleArgs(int I, G, V...)(G g, V v) {
   import std.traits: isAssignable;
   static if (V.length == 0) {
     return;
@@ -55,7 +147,7 @@ private void sample_args(int I, G, V...)(G g, V v) {
 		     " arguments for covergroup of type: " ~ G.stringof);
     }
     g.tupleof[N] = v[0];
-    sample_args!(I+1)(g, v[1..$]);
+    sampleArgs!(I+1)(g, v[1..$]);
   }
 }
 
@@ -73,6 +165,27 @@ private template getIntElements(G, int N=0, I...) {
     }
     else {
       enum getIntElements = getIntElements!(G, N+1, I);
+    }
+  }
+}
+
+private template findElementIndex(string E, G, int I=0, int N=0) {
+  import std.traits: isIntegral;
+  import esdl.data.bvec: isBitVector;
+  static if(I >= G.tupleof.length) {
+    enum findElementIndex = -1;
+  } else {
+    alias T = typeof(G.tupleof[I]);
+    static if (isBitVector!T || isIntegral!T || is(T == bool)) {
+      static if (E == G.tupleof[I].stringof) {
+	enum findElementIndex = N;
+      }
+      else {
+	enum findElementIndex =  findElementIndex!(E, G, I+1, N+1);
+      }
+    }
+    else {
+      enum findElementIndex = findElementIndex!(E, G, I+1, N);
     }
   }
 }
