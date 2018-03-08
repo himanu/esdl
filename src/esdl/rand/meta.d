541e89f347ef5157afcb688b585b08a4ff8bf418
Puneet Goel <puneet@coverify.com>
2018-03-08 19:40:11 +0530

    Renamed rand.rand module as rand.meta

--- src/esdl/rand/meta.d
+++ src/esdl/rand/meta.d
@@ -6,7 +6,7 @@
 //            http://www.boost.org/LICENSE_1_0.txt)
 // Authors:   Puneet Goel <puneet@coverify.com>
 
-module esdl.rand.rand;
+module esdl.rand.meta;
 
 import esdl.rand.obdd;
 
--- src/esdl/rand/package.d
+++ src/esdl/rand/package.d
@@ -4,5 +4,5 @@ public import esdl.rand.base;
 public import esdl.rand.cstx;
 public import esdl.rand.expr;
 public import esdl.rand.obdd;
-public import esdl.rand.rand;
+public import esdl.rand.meta;
 public import esdl.rand.solver;
