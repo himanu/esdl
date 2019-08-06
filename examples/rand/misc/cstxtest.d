0d58384d8c5b31c96efa9b4d06fda848c7dbc0b5
Puneet Goel <puneet@coverify.com>
2019-08-06 13:55:53 +0530

    Fixed constraint parser example

--- examples/rand/misc/cstxtest.d
+++ examples/rand/misc/cstxtest.d
@@ -3,19 +3,35 @@ import std.stdio;
 
 void main()
 {
-  writeln(constraintXlate(q{
+  writeln(constraintXlate("solver", q{
   	foreach (i, x; foo) {
   	  foo[i] == 0;
   	}
       }, __FILE__, __LINE__));
   
-  writeln(constraintXlate(q{
+  writeln(constraintXlate("solver", q{
 	a[0+4] == 0;
       }, __FILE__, __LINE__));
     
 
-  writeln(constraintXlate(q{
-  	a[0..4] == 0;
+  writeln(constraintXlate("solver", q{
+  	a == 4;
+      }, __FILE__, __LINE__));
+  
+  writeln(constraintXlate("solver", q{
+  	foreach (i, x; a) {
+  	  a[i] == 0;
+  	}
+      }, __FILE__, __LINE__));
+  
+  writeln(constraintXlate("solver", q{
+  	a.b == 4;
+      }, __FILE__, __LINE__));
+  
+  writeln(constraintXlate("solver", q{
+  	foreach (i, x; a) {
+  	  a[i].b == 0;
+  	}
       }, __FILE__, __LINE__));
   
 }
