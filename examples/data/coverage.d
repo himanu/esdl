0e011e5b2e3c3ca9458d7d959e832f5068a3350e
Puneet Goel <puneet@coverify.com>
2021-02-17 15:54:27 +0530

    A more detailed coverage example

--- examples/data/coverage.d
+++ examples/data/coverage.d
@@ -1,20 +1,31 @@
 import esdl.data;
+import esdl.rand;
 
-ubyte b;
 
 class Tx {
   mixin Randomization;
+  @rand ubyte b;
   @rand int c;
   @rand int z;
+  string s;
   Foo coverage;
 
-  class Foo: CoverGroup
+  @rand(false) class Foo: CoverGroup
   {
     int a;
-    CoverPoint!a cp_a;
+    CoverPoint!(a, q{
+	bins a = { [0:63],65 };
+	bins b[] = { [127:130],[137:147],200,[100:108] }; // note overlapping values
+	bins c[] = { 200,201,202 };
+	bins d = { [1000:$] };
+	// bins others[] = default;
+      }) cp_a;
+      
     CoverPoint!b cp_b;
     CoverPoint!c cp_c;
+    // CoverPoint!s cp_s;
   }
+
   this() {
     coverage = new Foo();
   }
@@ -26,10 +37,10 @@ void main()
 {
   import std.stdio;
   Tx tx = new Tx();
-  for (size_t i=0; i!=64; ++i) {
-    b = cast(ubyte) i;
+  for (size_t i=0; i!=256; ++i) {
+    // b = cast(ubyte) i;
     tx.randomize();
-    tx.coverage.sample(tx.z);
+    tx.coverage.sample(tx.z); // sample(tx.coverage, tx.c)
   }
   writeln(tx.coverage.cp_a.getBins());
   writeln(tx.coverage.cp_b.getBins());
