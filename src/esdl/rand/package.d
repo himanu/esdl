80f9b3c7eb927a7d951ab39b08a91962ab7e015d
Puneet Goel <puneet@coverify.com>
2021-02-19 16:16:58 +0530

    Split rand.func module from rand.expr

--- src/esdl/rand/package.d
+++ src/esdl/rand/package.d
@@ -5,6 +5,7 @@ public import esdl.rand.cstx;
 public import esdl.rand.cover;
 public import esdl.rand.base;
 public import esdl.rand.expr;
+public import esdl.rand.func;
 public import esdl.rand.dist;
 public import esdl.rand.meta;
 public import esdl.rand.proxy;
